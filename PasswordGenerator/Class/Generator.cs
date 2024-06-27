42e4597018e538822fca5ccc836f850acb40a07f
MrSatisfactionGuy <edd.wag18@gmail.com>
2024-06-27 19:40:26 +0200

    initial commit

---
+++ PasswordGenerator/Class/Generator.cs
@@ -0,0 +1,25 @@
+﻿namespace PasswordGenerator.Class
+{
+    public class Generator
+    {
+        private static readonly Random rnd = new Random();
+        private static string Buchstaben = "abcdefghijklmnopqrstuvwxyz";
+        private static string BuchstabenUpper = Buchstaben.ToUpper();
+        private static int Zahlen = 1234567890;
+        private static string Zeichen = "!@#$%^&*()_+";
+
+        static string combinedString = string.Format("{0}{1}{2}{3}", Buchstaben, BuchstabenUpper, Zahlen, Zeichen);
+
+        public string GeneratePassword(int lange)
+        {
+            char[] password = new char[lange];
+            for (int i = 0; i < lange; i++)
+            {
+                password[i] = combinedString[rnd.Next(combinedString.Length)];
+            }
+
+            return new string(password);
+        }
+
+    }
+}
---
+++ PasswordGenerator/Program.cs
@@ -0,0 +1,23 @@
+﻿using PasswordGenerator.Class;
+
+namespace PasswordGenerator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***Passwort Generator***");
+            Console.WriteLine("Bitte die Länge eingeben:");
+            string laenge = Console.ReadLine();
+
+            bool tryParseNum = false;
+
+            if (tryParseNum == Int32.TryParse(laenge, out int exitLength)) Console.WriteLine("Bitte eine Zahl angeben!");
+            Generator generator = new Generator();
+
+            string password = generator.GeneratePassword(exitLength);
+            Console.WriteLine(password);
+            Console.ReadKey();
+        }
+    }
+}
